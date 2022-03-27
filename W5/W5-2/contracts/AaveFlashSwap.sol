9875e6346e0577aec276b7dda0fbe59ae1b5d638
yucaijiu@gmail.com <yucaijiu@gmail.com>
2022-03-27 22:37:51 +0800

    5-2未完成的伪代码，及修改5-1

--- W5/W5-2/contracts/AaveFlashSwap.sol
+++ W5/W5-2/contracts/AaveFlashSwap.sol
@@ -2,15 +2,16 @@ pragma solidity ^0.8.0;
 
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Callee.sol';
 
-import '../libraries/UniswapV2Library.sol';
-import '../interfaces/V1/IUniswapV1Factory.sol';
-import '../interfaces/V1/IUniswapV1Exchange.sol';
-import '../interfaces/IUniswapV2Router01.sol';
-import '../interfaces/IERC20.sol';
-import '../interfaces/IWETH.sol';
-import '../interfaces/ISwapRouter.sol';
+import './libraries/UniswapV2Library.sol';
+import './interfaces/V1/IUniswapV1Factory.sol';
+import './interfaces/V1/IUniswapV1Exchange.sol';
+import './interfaces/IUniswapV2Router01.sol';
+import './interfaces/IERC20.sol';
+import './interfaces/IWETH.sol';
+import './interfaces/ISwapRouter.sol';
+import './interfaces/ISwapRouter.sol';
 
-contract FlashSwap is IUniswapV2Callee {
+contract AaveFlashSwap is IUniswapV2Callee, ISwapRouter,IFlashLoanSimpleReceiver{
 
     IUniswapV1Factory immutable factoryV1;
     address immutable factory;
@@ -28,7 +29,7 @@ contract FlashSwap is IUniswapV2Callee {
 
     // gets tokens/WETH via a V2 flash swap, swaps for the ETH/tokens on V1, repays V2, and keeps the rest!
     // 实现IUniswapV2Callee中uniswapV2Call方法，data看需要数据就传入什么数据
-    function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external override {
+    function executeOperation(    address asset,uint256 amount,uint256 premium,address initiator,bytes calldata params) external override {
         address[] memory path = new address[](2);
         //收到的token
         uint amountToken;
@@ -47,39 +48,36 @@ contract FlashSwap is IUniswapV2Callee {
         assert(path[0] == address(WETH) || path[1] == address(WETH)); // this strategy only works with a V2 WETH pair
         IERC20 token = IERC20(path[0] == address(WETH) ? path[1] : path[0]);
 
-        if (amountToken > 0) {
+
             (uint minETH) = abi.decode(data, (uint)); // slippage parameter for V1, passed in by caller
             // 去另外交易所套利
             IUniswapV1Exchange exchangeV1 = IUniswapV1Exchange(factoryV1.getExchange(address(token))); // get V1 exchange
-
+// 授权给 Pool 合约所借⾦额 + ⼿续费
             token.approve(address(exchangeV1), amountToken);
 
-        // 兑换
-        uint256 amountReceived = ISwapRouter(exchangeV1).exactInputSingle(
+        // token A 在 Uniswap V2 中交易兑换 token B
+        uint256 tokenB = ISwapRouter(exchangeV1).exactInputSingle(
             ISwapRouter.ExactInputSingleParams(token0,token1,0,address(this),block.timestamp,amountToken,0,0)
         );
 
-            uint amountRequired = UniswapV2Library.getAmountsIn(factory, amountToken, path)[0];
-            assert(amountReceived > amountRequired); // fail if we didn't get enough ETH back to repay our flash loan
-            //eth要转换weth
-            WETH.deposit{value: amountRequired}();
-            assert(WETH.transfer(msg.sender, amountRequired)); // return WETH to V2 pair
-            // 将amountReceived币发送到另外一个V2pair合约，谁调这个合约就发送给谁
-            (bool success,) = sender.call{value: amountReceived - amountRequired}(new bytes(0)); // keep the rest! (ETH)
-            assert(success);
-        } else {
-            //传入的最小token数量
-            (uint minTokens) = abi.decode(data, (uint)); // slippage parameter for V1, passed in by caller
-            WETH.withdraw(amountETH);
-            // 套利
-            uint amountReceived = exchangeV1.ethToTokenSwapInput{value: amountETH}(minTokens, uint(-1));
-            uint amountRequired = UniswapV2Library.getAmountsIn(factory, amountETH, path)[0];
-            assert(amountReceived > amountRequired); // fail if we didn't get enough tokens back to repay our flash loan
-            
+
+
+        // token B 在 Uniswap V3 中交易兑换 token A
+        uint256 kokenA = ISwapRouter(exchangeV1).exactInputSingle(
+            ISwapRouter.ExactInputSingleParams(token0,token1,0,address(this),block.timestamp,amountToken,0,0)
+        );
+
+
+        //返回tokenA还款给AAVE
+
+
+
+
             // 套利所得币重新返回调用者
             assert(token.transfer(msg.sender, amountRequired)); // return tokens to V2 pair
             //如果套利成功，就会把所得币转给调用者
             assert(token.transfer(sender, amountReceived - amountRequired)); // keep the rest! (tokens)
         }
     }
+    
 }
