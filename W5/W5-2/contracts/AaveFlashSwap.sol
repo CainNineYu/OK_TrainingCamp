3dd767c1db16dd3093bd7f5090ffc0bff8391a22
yucaijiu@gmail.com <yucaijiu@gmail.com>
2022-04-04 23:28:01 +0800

    5-2代码完善

--- W5/W5-2/contracts/AaveFlashSwap.sol
+++ W5/W5-2/contracts/AaveFlashSwap.sol
@@ -1,83 +1,60 @@
 pragma solidity ^0.8.0;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Callee.sol';
-
-import './libraries/UniswapV2Library.sol';
-import './interfaces/V1/IUniswapV1Factory.sol';
-import './interfaces/V1/IUniswapV1Exchange.sol';
 import './interfaces/IUniswapV2Router01.sol';
 import './interfaces/IERC20.sol';
-import './interfaces/IWETH.sol';
 import './interfaces/ISwapRouter.sol';
-import './interfaces/ISwapRouter.sol';
+import './interfaces/IPoolAddressesProvider.sol';
+import './interfaces/IPool.sol';
 
-contract AaveFlashSwap is IUniswapV2Callee, ISwapRouter,IFlashLoanSimpleReceiver{
+contract AaveFlashSwap is IFlashLoanSimpleReceiver{
 
     IUniswapV1Factory immutable factoryV1;
     address immutable factory;
     IWETH immutable WETH;
+    address public poolAddressesProvider;
+    address public routerV2;
+    address public routerV3;
+    address public Token1;//TokenB
+    IPoolAddressesProvider public immutable override ADDRESSES_PROVIDER;
+    IPool public immutable override POOL;
 
-    constructor(address _factory, address _factoryV1, address router) public {
-        factoryV1 = IUniswapV1Factory(_factoryV1);
-        factory = _factory;
-        WETH = IWETH(IUniswapV2Router01(router).WETH());
+    constructor( address _routerV2,  address _routerV3,address _poolAddressesProvider,address _token1,IFlashLoanSimpleReceiver) public {
+        poolAddressesProvider = _poolAddressesProvider;
+        routerV2 = _routerV2;
+        routerV3 = _routerV3;
+        token1 = _token1;
+        ADDRESSES_PROVIDER = provider;
+        POOL = IPool(provider.getPool());
     }
 
     // needs to accept ETH from any V1 exchange and WETH. ideally this could be enforced, as in the router,
     // but it's not possible because it requires a call to the v1 factory, which takes too much gas
-    receive() external payable {}
+    // receive() external payable {}
 
     // gets tokens/WETH via a V2 flash swap, swaps for the ETH/tokens on V1, repays V2, and keeps the rest!
     // 实现IUniswapV2Callee中uniswapV2Call方法，data看需要数据就传入什么数据
-    function executeOperation(    address asset,uint256 amount,uint256 premium,address initiator,bytes calldata params) external override {
+    function executeOperation(address asset, uint256 amount, uint256 premium, address initiator, bytes calldata params) external override returns (bool){
+        (address token0,) = abi.decode(params,(address,uint256));
+        uint returnMount = amount + premium;
         address[] memory path = new address[](2);
-        //收到的token
-        uint amountToken;
-        uint amountETH;
-        { // scope for token{0,1}, avoids stack too deep errors
-        address token0 = IUniswapV2Pair(msg.sender).token0();
-        address token1 = IUniswapV2Pair(msg.sender).token1();
-        assert(msg.sender == UniswapV2Library.pairFor(factory, token0, token1)); // ensure that msg.sender is actually a V2 pair
-        assert(amount0 == 0 || amount1 == 0); // this strategy is unidirectional
-        path[0] = amount0 == 0 ? token0 : token1;
-        path[1] = amount0 == 0 ? token1 : token0;
-        amountToken = token0 == address(WETH) ? amount1 : amount0;
-        amountETH = token0 == address(WETH) ? amount0 : amount1;
-        }
+        path[0] = token0;
+        path[1] = token1;
 
         assert(path[0] == address(WETH) || path[1] == address(WETH)); // this strategy only works with a V2 WETH pair
         IERC20 token = IERC20(path[0] == address(WETH) ? path[1] : path[0]);
+        //授权,token A 在 Uniswap V2 中交易兑换 token B
+        IERC20(token0).approve(routerV2, amount0);
+        uint[] memory tokenB = IUniswapV2Router01(routerV2).swapExactTokensForTokens(amount0,uint(0),path,address(this),block.timestamp+1000);
+        // 授权
+        IERC20(token1).approve(routerV3, tokenB[1]);
 
+        // token B 在 Uniswap V3 中交易兑换 token A        
+        ISwapRouter(routerV3).exactInput(0)(
+            ISwapRouter.ExactInputParams(abi.encodePacked(token1,uint24(1000),token1),address(this),block.timestamp + 1000,tokenB[1],0
+        ));
 
-            (uint minETH) = abi.decode(data, (uint)); // slippage parameter for V1, passed in by caller
-            // 去另外交易所套利
-            IUniswapV1Exchange exchangeV1 = IUniswapV1Exchange(factoryV1.getExchange(address(token))); // get V1 exchange
-// 授权给 Pool 合约所借⾦额 + ⼿续费
-            token.approve(address(exchangeV1), amountToken);
-
-        // token A 在 Uniswap V2 中交易兑换 token B
-        uint256 tokenB = ISwapRouter(exchangeV1).exactInputSingle(
-            ISwapRouter.ExactInputSingleParams(token0,token1,0,address(this),block.timestamp,amountToken,0,0)
-        );
-
-
-
-        // token B 在 Uniswap V3 中交易兑换 token A
-        uint256 kokenA = ISwapRouter(exchangeV1).exactInputSingle(
-            ISwapRouter.ExactInputSingleParams(token0,token1,0,address(this),block.timestamp,amountToken,0,0)
-        );
-
-
-        //返回tokenA还款给AAVE
-
-
-
-
-            // 套利所得币重新返回调用者
-            assert(token.transfer(msg.sender, amountRequired)); // return tokens to V2 pair
-            //如果套利成功，就会把所得币转给调用者
-            assert(token.transfer(sender, amountReceived - amountRequired)); // keep the rest! (tokens)
+        //授权给Pool合约地址,返回tokenA还款给AAVE
+        IERC20(token0).approve(ADDRESSES_PROVIDER.getPool(),returnMount);
+        return true;
         }
-    }
-    
 }
